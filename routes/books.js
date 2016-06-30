cf220575543b7c698c28471b337bb61e7e3787a1
Jesse Seligman <jesseseligman@gmail.com>
2016-06-30 14:12:46 -0700

    Changed stuff

--- routes/books.js
+++ routes/books.js
@@ -44,32 +44,38 @@ router.post('/books', (req, res, next) => {
     return res
       .status(400)
       .set('Content-Type', 'text/plain')
-      .send('title must not be blank');
+      .send('Title must not be blank');
   }
 
   if (!newBook.genre || newBook.genre.trim() === '') {
     return res
       .status(400)
       .set('Content-Type', 'text/plain')
-      .send('genre must not be blank');
+      .send('Genre must not be blank');
   }
 
   if (!newBook.description || newBook.description.trim() === '') {
     return res
       .status(400)
       .set('Content-Type', 'text/plain')
-      .send('description must not be blank');
+      .send('Description must not be blank');
   }
 
   if (!newBook.cover_url || newBook.cover_url.trim() === '') {
     return res
       .status(400)
       .set('Content-Type', 'text/plain')
-      .send('cover url must not be blank');
+      .send('Cover url must not be blank');
   }
 
   const authorId = Number.parseInt(newBook.author_id);
-  // check if id is number?
+
+  if (Number.isNaN(authorId)) {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('Author id is invalid');
+  }
 
   knex('authors')
     .where('id', authorId)
@@ -77,7 +83,9 @@ router.post('/books', (req, res, next) => {
     .then((author) => {
       if (!author) {
         return res
-          .sendStatus(400)
+          .status(400)
+          .set('Content-Type', 'text/plain')
+          .send('No author with that id exists');
       }
 
       knex('books')
@@ -92,22 +100,74 @@ router.post('/books', (req, res, next) => {
 });
 
 router.patch('/books/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
   knex('books')
-    .update(req.body, '*')
-    .where('id', req.params.id)
-    .then((books) => {
-      res.send(books[0]);
-    })
-    .catch((err) => {
-      next(err);
+    .where('id', id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return res
+          .status(400)
+          .set('Content-Type', 'text/plain')
+          .send('No book with that id exists');
+      }
+
+      const authorId = req.body.author_id || book.author_id;
+
+      if (Number.isNaN(authorId)) {
+        return res
+          .status(400)
+          .set('Content-Type', 'text/plain')
+          .send('Author id is invalid');
+      }
+
+      knex('authors')
+        .where(id, authorId)
+        .first()
+        .then((author) => {
+          if (!author) {
+            return res
+              .status(400)
+              .set('Content-Type', 'text/plain')
+              .send('No author with that id exists');
+          }
+
+          if (req.)
+        })
+
+      knex('books')
+        .update(req.body, '*')
+        .where('id', req.params.id)
+        .then((books) => {
+          res.send(books[0]);
+        })
+        .catch((err) => {
+          next(err);
+        });
     });
 });
 
 router.delete('/books/:id', (req, res, next) => {
+  const id = Number.parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return next();
+  }
+
   knex('books')
     .where('id', req.params.id)
     .first()
     .then((book) => {
+      if (!book) {
+        return res
+          .status(400)
+          .set('Content-Type', 'text/plain')
+          .send('No book with that id exists')
+      }
 
       return knex('books').del()
         .where('id', req.params.id)
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,61 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
 
+router.post('/users', (req, res, next) => {
+  const newUser = req.body;
 
+  if (!newUser.email || newUser.email.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('Email must not be blank');
+  }
+
+  if (!newUser.password || newUser.password.trim() === '') {
+    return res
+      .status(400)
+      .set('Content-Type', 'text/plain')
+      .send('Password must not be blank');
+  }
+
+  knex('users')
+    .select(knex.raw('1=1'))
+    .where('email', newUser.email)
+    .first()
+    .then((exists) => {
+      if (exists) {
+        return res
+        .status(400)
+        .set('Content-Type', 'text/plain')
+        .send('Email already exists')
+      }
+
+      bcrypt.hash(newUser.password, 12, (hashErr, hashed_password) => {
+        if (hashErr) {
+          return next(hashErr);
+        }
+
+        knex('users')
+          .insert({
+            first_name: newUser.first_name,
+            last_name: newUser.last_name,
+            email: newUser.email,
+            hashed_password: hashed_password
+          })
+          .then(() => {
+            res.sendStatus(200);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
